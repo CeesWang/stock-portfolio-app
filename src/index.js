6d78f418a1d592f5621152f5f8b6a1ae5846afca
Cees Wang <wangcees@gmail.com>
2020-02-17 18:32:18 -0500

    fixed node modules error for employing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-
+require('dotenv').config();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
